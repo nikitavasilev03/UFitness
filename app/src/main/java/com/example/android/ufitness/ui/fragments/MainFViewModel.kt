51f5a7aa6c9b0fae1c5921182986aaa8ae1de975
SomethingBringer <verannick@gmail.com>
2020-11-02 22:13:01 +0300

    start fragment

--- app/src/main/java/com/example/android/ufitness/ui/fragments/MainFViewModel.kt
+++ app/src/main/java/com/example/android/ufitness/ui/fragments/MainFViewModel.kt
@@ -1,8 +1,9 @@
 package com.example.android.ufitness.ui.fragments
 
 import androidx.lifecycle.ViewModel
+import com.example.android.ufitness.utils.DataSource
 import javax.inject.Inject
 
-class MainFViewModel @Inject constructor(): ViewModel() {
+class MainFViewModel @Inject constructor(val dataSource: DataSource): ViewModel() {
 
 }
--- app/src/main/java/com/example/android/ufitness/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/android/ufitness/ui/fragments/MainFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.ufitness.MyApplication
 import com.example.android.ufitness.R
+import com.example.android.ufitness.models.Plan
 import javax.inject.Inject
 
 class MainFragment : Fragment() {
@@ -15,18 +16,40 @@ class MainFragment : Fragment() {
     @Inject
     lateinit var providerFactory: ViewModelProvider.Factory
     lateinit var viewModel: MainFViewModel
+    lateinit var plansAdapter: PlansAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity?.applicationContext as MyApplication).appComponent.inject(this)
-        viewModel = ViewModelProvider(this,providerFactory).get(MainFViewModel::class.java)
+        viewModel = ViewModelProvider(this, providerFactory).get(MainFViewModel::class.java)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        plansAdapter = PlansAdapter(
+                supportClick = ::supportClick,
+                deleteClick = ::deleteClick,
+                updateClick = ::updateClick
+        )
+    }
+
+    private fun supportClick(plan: Plan) {
+
+    }
+
+    private fun deleteClick(plan: Plan) {
+
+    }
+
+    private fun updateClick(plan: Plan) {
+
     }
 
 
