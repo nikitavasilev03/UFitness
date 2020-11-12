09d6d13daa100594604ad305df8b3e2757c72642
SomethingBringer <verannick@gmail.com>
2020-11-12 18:00:39 +0300

    db update
    new table and models update

--- app/src/main/java/com/example/android/ufitness/db/AppDatabase.kt
+++ app/src/main/java/com/example/android/ufitness/db/AppDatabase.kt
@@ -4,21 +4,25 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.android.ufitness.db.AppDatabase.Companion.DB_VERSION
 import com.example.android.ufitness.db.dao.ExerciseDao
+import com.example.android.ufitness.db.dao.ExercisePlansDao
 import com.example.android.ufitness.db.dao.PlanDao
 import com.example.android.ufitness.models.Exercise
+import com.example.android.ufitness.models.ExercisePlans
 import com.example.android.ufitness.models.Plan
 
 @Database(
-    entities =[
+    entities = [
         Exercise::class,
-        Plan::class
+        Plan::class,
+        ExercisePlans::class
     ], version = DB_VERSION
 )
 abstract class AppDatabase : RoomDatabase() {
     companion object {
-        const val DB_VERSION = 1
+        const val DB_VERSION = 3
     }
 
     abstract fun exerciseDao(): ExerciseDao
     abstract fun planDao(): PlanDao
+    abstract fun exercisePlansDao(): ExercisePlansDao
 }
--- app/src/main/java/com/example/android/ufitness/models/Exercise.kt
+++ app/src/main/java/com/example/android/ufitness/models/Exercise.kt
@@ -1,6 +1,7 @@
 package com.example.android.ufitness.models
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
@@ -8,8 +9,8 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 @Entity(tableName = "exercises")
 data class Exercise(
-        @PrimaryKey(autoGenerate = true)
-        val id: Int? = null,
-        val name: String,
-        val description: String
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val id: Int? = null,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "description") val description: String
 ) : Parcelable
