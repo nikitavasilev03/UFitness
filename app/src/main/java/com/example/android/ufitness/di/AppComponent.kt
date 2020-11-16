4dfc32ba45dd06c49002861e273807941702b1fc
SomethingBringer <verannick@gmail.com>
2020-11-16 21:04:31 +0300

    view model

--- app/src/main/java/com/example/android/ufitness/di/AppComponent.kt
+++ app/src/main/java/com/example/android/ufitness/di/AppComponent.kt
@@ -1,8 +1,8 @@
 package com.example.android.ufitness.di
 
+import com.example.android.ufitness.ui.fragments.support.SupportFragment
 import com.example.android.ufitness.ui.fragments.exercises.ExercisesFragment
 import com.example.android.ufitness.ui.fragments.exercises.edit.EditExerciseFragment
-import com.example.android.ufitness.ui.fragments.main.MainFViewModel
 import com.example.android.ufitness.ui.fragments.main.MainFragment
 import com.example.android.ufitness.ui.fragments.plans.PlansFragment
 import com.example.android.ufitness.ui.fragments.plans.edit.EditPlanFragment
@@ -18,5 +18,5 @@ interface AppComponent {
     fun inject(fragment: EditPlanFragment)
     fun inject(fragment: ExercisesFragment)
     fun inject(fragment: EditExerciseFragment)
-
+    fun inject(fragment: SupportFragment)
 }
