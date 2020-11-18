ba0675d6c1bfc4ad40be546c9541415b4475a0cb
Никита Васильев <nikitavasilev2303@gmail.com>
2020-11-18 19:41:08 +0300

    допилено изменение планов

---
+++ app/src/main/java/com/example/android/ufitness/ui/fragments/plans/edit/ExerciseSittingAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.android.ufitness.ui.fragments.plans.edit
+
+import com.example.android.ufitness.models.Exercise
+import com.example.android.ufitness.models.ExercisePlans
+
+class ExersiseSittingAdapter(
+    private var exercise: Exercise,
+    private var exercisePlan: ExercisePlans
+){
+
+}
---
+++ app/src/main/java/com/example/android/ufitness/ui/fragments/plans/edit/measurementDialog/MeasurementDialog.kt
@@ -0,0 +1,2 @@
+package com.example.android.ufitness.ui.fragments.plans.edit.measurementDialog
+
